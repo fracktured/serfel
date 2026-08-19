1b01a51f74391a08f162ab14bb975fbaa05e82bb
Christian Castro <christian.castro.ex@copec.cl>
2026-08-18 20:02:40 -0400

    feat(pedidos): show discount tramos and cap discount by quantity in detalle modal

--- apps/legacy-frontend/src/app/pages/pedidos/modal-detalle-producto/modal-detalle-producto.component.ts
+++ apps/legacy-frontend/src/app/pages/pedidos/modal-detalle-producto/modal-detalle-producto.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { PrecioProductoModel } from '../../../models/precio-producto.model';
+import { getTramosActivos, getTechoEfectivo, getTramoActivoCant, Tramo } from './tramos';
 
 @Component({
   selector: 'app-modal-detalle-producto',
@@ -11,9 +12,31 @@ export class ModalDetalleProductoComponent implements OnInit {
 
   @Input() producto: PrecioProductoModel;
 
+  tramos: Tramo[] = [];
+  tieneTramos = false;
+  techoEfectivo = 0;
+  tramoActivoCant: number | null = null;
+
   constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit(): void {
+    this.tramos = getTramosActivos(this.producto);
+    this.tieneTramos = this.tramos.length > 0;
+    this.recalcularTecho();
+  }
+
+  onCantidadChange(): void {
+    this.recalcularTecho();
+    const porcen = Number(this.producto.porcenDesc);
+    if (!isNaN(porcen) && porcen > this.techoEfectivo) {
+      this.producto.porcenDesc = this.techoEfectivo;
+    }
+  }
+
+  private recalcularTecho(): void {
+    const cantidad = Number(this.producto.cantidad) || 0;
+    this.techoEfectivo = getTechoEfectivo(this.producto, cantidad);
+    this.tramoActivoCant = getTramoActivoCant(this.producto, cantidad);
   }
 
   ingresarDetalleProducto() {
@@ -32,14 +55,14 @@ export class ModalDetalleProductoComponent implements OnInit {
     this.producto.cantidadStock = parseFloat(this.producto.cantidadStock.toString());
     this.producto.cantidadPedida = parseFloat(this.producto.cantidadPedida.toString());
 
+    const techo = getTechoEfectivo(this.producto, this.producto.cantidad);
+
     if ( this.producto.cantidad > (this.producto.cantidadStock - this.producto.cantidadPedida) ) {
-      // tituloModalMensajes = 'Supero el porcentaje de descuento';
       alert('El stock maximo para la venta es: ' + (this.producto.cantidadStock - this.producto.cantidadPedida));
       return;
     }
-    else if ( this.producto.porcenDesc > this.producto.maxPorcenDesc ) {
-      // tituloModalMensajes = 'Supero la cantidad maxima de stock';
-      alert('El porcentaje maximo de descuento que puede utilizar es: ' + this.producto.maxPorcenDesc);
+    else if ( this.producto.porcenDesc > techo ) {
+      alert('El porcentaje maximo de descuento que puede utilizar es: ' + techo);
       return;
     }
     this.activeModal.close(this.producto);
